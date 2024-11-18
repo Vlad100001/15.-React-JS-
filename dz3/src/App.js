deeea2fa88735e578c63d4bddde12b0a8405cbfb
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2024-11-18 16:28:58 +0300

    Update App.js

--- dz3/src/App.js
+++ dz3/src/App.js
@@ -1,9 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './Component/Greeting';
-import Counter from './Component/Counter';
-// import MessagesList from './Component/MessagesList';
-// import { messages } from './messages'; // Импортируем массив сообщений
 import TemperatureConverter from './Component/dz3-1';
 import TodoList from './Component/dz3-2';
 
@@ -13,11 +9,7 @@ function App() {
   const name = "Alex"
   return (
     <div className='App'>
-      {/* <h1> Leant react</h1> */}
-      {/* <Greeting name={name} />
-      <Counter /> */}
-      {/* <MessagesList messages={messages} /> */}
-      <h1>Температурный конвертер</h1>
+       <h1>Температурный конвертер</h1>
       <TemperatureConverter />
 
       <h1>Список дел</h1>
@@ -27,7 +19,6 @@ function App() {
 
   );
 }
-
 
 export default App;
 
